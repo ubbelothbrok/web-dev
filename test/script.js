b2516930ea7fead3791635cb06c3d5ea5c710d02
ubbelothbrok <adityavishwakarma299@gmail.com>
2025-04-02 10:47:58 +0530

    first commit

---
+++ test/script.js
@@ -0,0 +1,15 @@
+console.log(5+9);
+
+console.log(`hello world`)
+console.log("A newline character is written like \"\\n\".")
+console.log("hello"+" aditya")
+console.log(`hello ${5654*6544}`)
+
+console.log(typeof "-44")
+a="hello"
+console.log(a)
+console.log(NaN!=NaN)
+
+
+b=1 + 1 == 2 && 10 * 10 < 50
+console.log(b ? 1 : 2)
